3ef609e9e4d2c8b1984c606935ff3698390834ae
milan-dev <milan.herke.business@gmail.com>
2020-01-09 19:34:45 +0100

    Update 0.0.2

--- app/src/main/java/app/milanherke/mystudiez/MainActivity.kt
+++ app/src/main/java/app/milanherke/mystudiez/MainActivity.kt
@@ -1,6 +1,8 @@
 package app.milanherke.mystudiez
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -10,12 +12,50 @@ import com.google.android.material.bottomappbar.BottomAppBar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        this.deleteDatabase("MyStudiez.db")
+
+        val appDatabase = AppDatabase.getInstance(this)
+        val db = appDatabase.readableDatabase
+
+
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testInsert()
+        testUpdate()
+        testDelete()
+
+        val cursor = db.rawQuery("SELECT * FROM Subjects", null)
+        Log.i(TAG, "*************************")
+        cursor.use {
+            while (it.moveToNext()) {
+                with(cursor) {
+                    val id = getLong(0)
+                    val name = getString(1)
+                    val teacher = getString(2)
+                    val colorcode = getString(3)
+                    val result = "Id: $id. Name: $name. Teacher: $teacher. Color code: $colorcode"
+                    Log.i(TAG, "onCreate: reading data: $result")
+                }
+            }
+        }
+        Log.i(TAG, "*************************")
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -27,6 +67,35 @@ class MainActivity : AppCompatActivity() {
         bottomBar.setNavigationOnClickListener {
             Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun testInsert() {
+        val values = ContentValues().apply {
+            put(SubjectsContract.Columns.SUBJECT_NAME, "Subject 1")
+            put(SubjectsContract.Columns.SUBJECT_TEACHER, "Teacher 1")
+            put(SubjectsContract.Columns.SUBJECT_COLORCODE, "Color code 1")
+        }
+
+        val uri = contentResolver.insert(SubjectsContract.CONTENT_URI, values)
+        Log.i(TAG, "New row id (in uri) is $uri")
+        Log.i(TAG, "id (in uri) is ${TasksContract.getId(uri)}")
+    }
+
+    private fun testUpdate() {
+        val values = ContentValues().apply {
+            put(SubjectsContract.Columns.SUBJECT_NAME, "Subject 11111")
+            put(SubjectsContract.Columns.SUBJECT_TEACHER, "Teacher 121")
+            put(SubjectsContract.Columns.SUBJECT_COLORCODE, "Color code 132")
+        }
+        val subjectUri = SubjectsContract.buildUriFromId(5)
+        val uri = contentResolver.update(subjectUri, values, null, null)
+        Log.i(TAG, "Row with the id ${SubjectsContract.buildUriFromId(5)} is updated.")
+    }
+
+    private fun testDelete() {
+        val subjectUri = SubjectsContract.buildUriFromId(6)
+        val uri = contentResolver.delete(subjectUri, null, null)
+        Log.i(TAG, "Row with the id 6 is deleted")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
